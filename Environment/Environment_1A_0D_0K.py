ff2a0522c48e0aaabd017631da264dd9063ef23c
G.Uttej Kumar Reddy <uttej60@gmail.com>
2023-10-27 10:14:31 -0700

    Run simulation through contacts until timer runs out

--- Environment/Environment_1A_0D_0K.py
+++ Environment/Environment_1A_0D_0K.py
@@ -132,12 +132,12 @@ class Environment_1A_0D_0K():
 				# Get new observation
 				new_observation = self.get_observation_space(data)
 
-				# Check if ball is kicked
 				done = False
-				contacts = data.contact
-				for c in contacts:
-					if c.geom1 == self.ball.id_geom and c.geom2 == self.player.id_geom:
-						done = True	
+				# Check if ball is kicked
+				# contacts = data.contact
+				# for c in contacts:
+				# 	if c.geom1 == self.ball.id_geom and c.geom2 == self.player.id_geom:
+				# 		done = True	
 				
 				info = {}
 
