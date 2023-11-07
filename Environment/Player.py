8b9356ebed5b5bc94675e65a8a633ba73bfac6fb
G.Uttej Kumar Reddy <uttej60@gmail.com>
2023-11-06 20:34:04 -0800

    Experiment 14 - Start

--- Environment/Player.py
+++ Environment/Player.py
@@ -75,9 +75,22 @@ class Player:
 			reward_vel_to_ball = np.dot(player_velocity, player_to_ball_unit_vector)
 
 			# vel-ball-to-goal: ball's linear velocity projected onto its unit direction vector towards the center of the opponent's goal
-			# field_config = envProps.OBSERVATION_SPACE[os.environ['SOCCER_DIMS']]
-			# goal_position = (-5.7375, 0)
-			# ball_to_goal_unit_vector = (goal_position - ball_position) / np.linalg.norm(goal_position - ball_position)
-			# reward_vel_ball_to_goal = np.dot(ball_velocity, ball_to_goal_unit_vector)
+			goal_position = (-5.7375, 0)
+			ball_to_goal_unit_vector = (goal_position - ball_position) / np.linalg.norm(goal_position - ball_position)
+			reward_vel_ball_to_goal = np.dot(ball_velocity, ball_to_goal_unit_vector)
 
-			return 0.05 * reward_vel_to_ball # + 0.1 * reward_vel_ball_to_goal
+			# goal: the ball touches the back net
+			geom_goal_to_score_in_blue_team = mj.mj_name2id(data, mj.mjtObj.mjOBJ_GEOM, 'goalE_W')
+			geom_goal_to_score_in_red_team = mj.mj_name2id(data, mj.mjtObj.mjOBJ_GEOM, 'goalW_E')
+
+			reward_goal = 0
+			contacts = data.contact
+			for c in contacts:
+				if (c.geom1 == ball.id_geom and c.geom2 == geom_goal_to_score_in_blue_team) or \
+					(c.geom1 == geom_goal_to_score_in_blue_team and c.geom2 == ball.id_geom):
+					reward_goal = 1
+				if (c.geom1 == ball.id_geom and c.geom2 == geom_goal_to_score_in_red_team) or \
+					(c.geom1 == geom_goal_to_score_in_red_team and c.geom2 == ball.id_geom):
+					reward_goal = -1
+
+			return (reward_goal + (0.05 * reward_vel_to_ball) + (0.1 * reward_vel_ball_to_goal))
